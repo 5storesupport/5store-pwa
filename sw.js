63d76c86beb2140d94ec8e9c9dc1d0f056b14077
5storesupport <5store.support@gmail.com>
2026-09-23 00:21:54 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = '5store-static-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+
+  if (req.method !== 'GET') return;
+
+  const url = new URL(req.url);
+
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(req).catch(() => caches.match(req))
+  );
+});
